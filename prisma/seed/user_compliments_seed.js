57b3e523404b990b5c32f7e38fdcd671054dd704
CalebScott0 <calebbenscott@gmail.com>
2024-10-11 00:13:40 -0500

    seed user compliments hosted db

---
+++ prisma/seed/user_compliments_seed.js
@@ -0,0 +1,139 @@
+const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const prisma = new PrismaClient({
+  log: ["info"],
+});
+let complimentsArr = [];
+let records;
+let count = 0;
+//
+async function processCSV() {
+  // file will not be in github as it is part of yelp academic dataset
+  const parser = fs
+    .createReadStream(
+      "/Users/cbs062/Desktop/Review_Site_CSV_Files/user_compliment.csv"
+    )
+    .pipe(parse({ from_line: 2 }));
+
+  // compliment table has:
+  // id - default uuid
+  // user_id
+  // enum type
+  /* - COOL
+   * - CUTE
+   * - FUNNY
+   * -  HOT
+   * - LIST
+   * - MORE
+   * - NOTE
+   * - PHOTOS
+   * - PLAIN
+   * - PROFILE
+   * - WRITER
+   */
+  // count
+
+  const ENUMS = [
+    "COOL",
+    "CUTE",
+    "FUNNY",
+    "HOT",
+    "LIST",
+    "MORE",
+    "NOTE",
+    "PHOTOS",
+    "PLAIN",
+    "PROFILE",
+    "WRITER",
+  ];
+
+  const BATCH_SIZE = 100;
+
+  for await (const record of parser) {
+    // complimentsArr.push(record);
+    complimentsArr.push({
+      id: record[0],
+      user_id: record[1],
+      type: record[2],
+      count: +record[3],
+    });
+    if (complimentsArr.length === BATCH_SIZE) {
+      try {
+        await prisma.user_compliment.createMany({
+          data: [...complimentsArr.splice(0, complimentsArr.length)],
+          skipDuplicates: true,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(`${(count += BATCH_SIZE)} records created`);
+    }
+  }
+  return;
+  // flatMap to return one array instead of array of nested arrays
+  records = complimentsArr.flatMap((user) => {
+    //user_id is the first index of each nested user array - rest of the indexes are counts of compliments
+    const user_id = user[0];
+    //   for each user, return a mapped array of objects
+    //   each object will consist of {
+    //      user_id
+    //      enum type of compliment
+    //      count of the compliment type received
+    // }
+    return ENUMS.map((type, i) => ({
+      user_id,
+      type,
+      count: +user[i + 1],
+    }));
+  });
+  // each nested array represents 1 user and their
+  // received compliments
+
+  const create_batch = [];
+  console.log("Creating records...");
+  for (let i = 0; i < records.length; i++) {
+    create_batch.push(records[i]);
+
+    if (create_batch.length === BATCH_SIZE) {
+      try {
+        await prisma.user_compliment.createMany({
+          data: [...create_batch.splice(0, BATCH_SIZE)],
+          skipDuplicates: true,
+        });
+
+        console.log(
+          `${(count += BATCH_SIZE)} records created / ${records.length}`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  if (create_batch.length > 0) {
+    try {
+      await prisma.user_compliment.createMany({
+        data: create_batch,
+        skipDuplicates: true,
+      });
+
+      console.log(
+        `${(count += create_batch.length)} records created / ${records.length}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log("Completed");
+}
+
+processCSV()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+
+    await prisma.$disconnect();
+  });
--- prisma/seed/user_friends_seed.js
+++ prisma/seed/user_friends_seed.js
@@ -5,8 +5,7 @@ const { parse } = require("csv-parse");
 const prisma = new PrismaClient({
   log: ["info"],
 });
-let records = [];
-// let friendsArr = [];
+let friendsArr = [];
 let count = 0;
 //
 async function processCSV() {
@@ -18,91 +17,60 @@ async function processCSV() {
     .pipe(parse({ from_line: 2 }));
 
   for await (const record of parser) {
-    records.push({
-      user_id: record[0],
-      friend_id: record[1],
-      friends_since: record[2],
-    });
+    const user_id = record[0];
 
-    const BATCH_SIZE = 100;
-    if (records.length === BATCH_SIZE) {
-      try {
-        await prisma.user_friend.createMany({
-          data: [...records.splice(0, records.length)],
-          skipDuplicates: true,
+    const user_friends = record[1].split(", ");
+
+    for (const friend_id of user_friends) {
+      // if (!set.has(friend_id)) {
+      const friend_exists = await prisma.user.findUnique({
+        where: { id: friend_id },
+      });
+      // }
+
+      if (!friend_exists) {
+        continue;
+        // Skip this friend if they don't exist
+      } else {
+        // set.add(friend_id);
+        friendsArr.push({
+          user_id,
+          friend_id,
         });
-        count += BATCH_SIZE;
-        console.log(`${count} records created`);
-      } catch (error) {
-        console.log(error);
       }
     }
-    // const user_id = record[0];
 
-    // const user_friends = record[1].split(", ");
-
-    // for (const friend_id of user_friends) {
-    // add all unique user ids to the set
-    // avoid millions of user find unique calls
-    // if (!set.has(friend_id)) {
-    // const friend_exists = await prisma.user.findUnique({
-    //   where: { id: friend_id },
-    // });
-
-    // if (!friend_exists) {
-    // continue;
-    // Skip this friend if they don't exist
-    // } else {
-    // set.add(friend_id);
-    // friendsArr.push({
-    // user_id,
-    // friend_id,
-    // });
-    // }
-    // }
-
-    //   const BATCH_SIZE = 100;
-    // if (friendsArr.length === BATCH_SIZE) {
-    //   try {
-    //     await prisma.user_friend
-    //       .createMany({
-    //         data: [...friendsArr.splice(0, friendsArr.length)],
-    //         skipDuplicates: true, // To avoid duplicates if the seeding is rerun
-    //       })
-    //       .then(() => (count += BATCH_SIZE))
-    //         .then(() => console.log(`${count} friends created`));
-    //     } catch (error) {
-    //       console.error("Unable to create batch:", error);
-    //     }
-    //   }
-    // }
-    // }
-    // remaining records
-    // if (friendsArr.length > 0) {
-    //   try {
-    //     await prisma.$transaction(async (prisma) => {
-    //       await prisma.user_friend.createMany({
-    //         data: friendsArr,
-    //         skipDuplicates: true, // To avoid duplicates if the seeding is rerun
-    //       });
-    //     });
-
-    //     console.log(`Inserted ${friendsArr.length} records`);
-    //   } catch (error) {
-    //     console.error("Unable to create batch:", error);
-    //   }
-  }
-  if (records.length > 0) {
-    try {
-      await prisma.user_friend.createMany({
-        data: records,
-        skipDuplicates: true,
-      });
-    } catch (error) {
-      console.log(error);
+    const BATCH_SIZE = 100;
+    if (friendsArr.length === BATCH_SIZE) {
+      try {
+        await prisma.user_friend
+          .createMany({
+            data: [...friendsArr.splice(0, friendsArr.length)],
+            skipDuplicates: true, // To avoid duplicates if the seeding is rerun
+          })
+          .then(() => (count += BATCH_SIZE))
+          .then(() => console.log(`${count} friends created`));
+      } catch (error) {
+        console.error("Unable to create batch:", error);
+      }
     }
   }
-  console.log("Complete");
+  // }
+  // remaining records
+  if (friendsArr.length > 0) {
+    try {
+      await prisma.user_friend.createMany({
+        data: friendsArr,
+        skipDuplicates: true, // To avoid duplicates if the seeding is rerun
+      });
+
+      console.log(`Inserted ${friendsArr.length} records`);
+    } catch (error) {
+      console.error("Unable to create batch:", error);
+    }
+
+    console.log("Complete");
+  }
 }
 
 processCSV()
