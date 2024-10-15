b76db939262c85ec5ef494c84e6d9d905a450f21
CalebScott0 <calebbenscott@gmail.com>
2024-10-15 18:23:51 -0500

    creating business attributes tables

--- api/category.js
+++ api/category.js
@@ -4,7 +4,7 @@ const category_router = express.Router();
 const { getCategories } = require("../db/category");
 
 // GET category/all_categories - returns all distinct categories of current businesses
-category_router.get("/all_categories", async (req, res, next) => {
+category_router.get("/list/all_categories", async (req, res, next) => {
   try {
     const categories = await getCategories();
     res.send({ categories });
