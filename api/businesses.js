ef4f83e37a756ab8a9fc31f3469a1f3eb90cf969
CalebScott0 <calebbenscott@gmail.com>
2024-10-21 13:56:54 -0500

    combined location and category endpoint for business listings

--- api/businesses.js
+++ api/businesses.js
@@ -36,48 +36,48 @@ business_router.get("/locations", async (req, res, next) => {
 // function can take latitude, longitude, and a radius
 // has a default in db query
 // GET api/businesses/nearby?city=""&state=""limit={&offset={}
-business_router.get("/nearby", async (req, res, next) => {
-  try {
-    // get city, state from req query and use location iq api
-    const { city, state, limit, offset } = req.query;
-    // make call to location iq api with req query city and state, returns 1
-    const url = `https://us1.locationiq.com/v1/search/structured?city=${city}&state=${state}&format=json&limit=1&key=${process.env.LOCATION_API_KEY}`;
-    const options = { method: "GET", headers: { accept: "application/json" } };
-    // use node fetch to call location iq api
-    const location_response = await fetch(url, options);
-    const json = await location_response.json();
-    // pass longitude and latitude from location iq to get all businesses from location function
-    // default radius parameter of 10miles converted to kilometers
-    const fetch_businesses = await getAllBusinessesFromLocation({
-      limit: +limit,
-      offset: +offset,
-      longitude: +json[0].lon,
-      latitude: +json[0].lat,
-    });
+// business_router.get("/nearby", async (req, res, next) => {
+//   try {
+//     // get city, state from req query and use location iq api
+//     const { city, state, limit, offset } = req.query;
+//     // make call to location iq api with req query city and state, returns 1
+//     const url = `https://us1.locationiq.com/v1/search/structured?city=${city}&state=${state}&format=json&limit=1&key=${process.env.LOCATION_API_KEY}`;
+//     const options = { method: "GET", headers: { accept: "application/json" } };
+//     // use node fetch to call location iq api
+//     const location_response = await fetch(url, options);
+//     const json = await location_response.json();
+//     // pass longitude and latitude from location iq to get all businesses from location function
+//     // default radius parameter of 10miles converted to kilometers
+//     const fetch_businesses = await getAllBusinessesFromLocation({
+//       limit: +limit,
+//       offset: +offset,
+//       longitude: +json[0].lon,
+//       latitude: +json[0].lat,
+//     });
 
-    const businesses = await Promise.all(
-      // Get most recent review for business (review db query limits to 1 on default and ordered by created_at)
-      // get categories / hours for business
-      fetch_businesses.map(async (business) => {
-        let [review, business_hours, categories] = await Promise.all([
-          getReviewsForBusiness({ business_id: business.id }),
-          getBusinessHours(business.id),
-          getCategoriesForBusiness(business.id),
-        ]);
-        return {
-          ...business,
-          hours: business_hours,
-          categories,
-          recent_review: review[0],
-        };
-      })
-    );
+//     const businesses = await Promise.all(
+//       // Get most recent review for business (review db query limits to 1 on default and ordered by created_at)
+//       // get categories / hours for business
+//       fetch_businesses.map(async (business) => {
+//         let [review, business_hours, categories] = await Promise.all([
+//           getReviewsForBusiness({ business_id: business.id }),
+//           getBusinessHours(business.id),
+//           getCategoriesForBusiness(business.id),
+//         ]);
+//         return {
+//           ...business,
+//           hours: business_hours,
+//           categories,
+//           recent_review: review[0],
+//         };
+//       })
+//     );
 
-    res.send({ businesses });
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
-});
+//     res.send({ businesses });
+//   } catch ({ name, message }) {
+//     next({ name, message });
+//   }
+// });
 
 // Get a list of businesses with a given category_id
 // GET /api/businesses/categories/:category_id?city=""&state=""&limit={}&offset={}
