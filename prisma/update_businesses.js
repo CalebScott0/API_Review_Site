50f6b65661140cb2625fdd77d531af2569e080d8
CalebScott0 <calebbenscott@gmail.com>
2024-10-28 21:11:28 -0500

    seeding random business hours to businesses with no hours

--- prisma/update_businesses.js
+++ prisma/update_businesses.js
@@ -76,44 +76,45 @@ const getRandomTime = (min_hour, max_hour) => {
     });
   }
 
-  // console.log(
-  //   `Updating ${businesses.length} businesses with review count and average stars...`
-  // );
-  // // group reviews by business_id average on stars and total count
-  // const business_stats = await prisma.reviews.groupBy({
-  //   by: ["business_id"],
-  //   _avg: {
-  //     stars: true, // Calculate the average stars
-  //   },
-  //   _count: {
-  //     stars: true, // Count the total number of reviews
-  //   },
-  // });
-  // const BATCH_SIZE = 100;
-  // for (let i = 0; i < business_stats.length; i += BATCH_SIZE) {
-  //   // create array of business promises
-  //   const update_batch = business_stats
-  //     .slice(i, i + BATCH_SIZE)
-  //     .map((business) =>
-  //       prisma.businesses.update({
-  //         where: {
-  //           id: business.business_id,
-  //         },
-  //         data: {
-  //           average_stars: +business._avg.stars.toFixed(2),
-  //           review_count: business._count.stars,
-  //         },
-  //       })
-  //     );
-  //   await Promise.all(update_batch);
-  //   console.log(
-  //     `Updated business # ${i} / ${business_stats.length} - ${(
-  //       (i / businesses.length) *
-  //       100
-  //     ).toFixed(2)}%...`
-  //   );
-  // }
-  // console.log("Business Update Complete");
+  const businesses = await prisma.$queryRaw`SELECT * FROM businesses`;
+  console.log(
+    `Updating ${businesses.length} businesses with review count and average stars...`
+  );
+  // group reviews by business_id average on stars and total count
+  const business_stats = await prisma.reviews.groupBy({
+    by: ["business_id"],
+    _avg: {
+      stars: true, // Calculate the average stars
+    },
+    _count: {
+      stars: true, // Count the total number of reviews
+    },
+  });
+  const BATCH_SIZE = 100;
+  for (let i = 0; i < business_stats.length; i += BATCH_SIZE) {
+    // create array of business promises
+    const update_batch = business_stats
+      .slice(i, i + BATCH_SIZE)
+      .map((business) =>
+        prisma.businesses.update({
+          where: {
+            id: business.business_id,
+          },
+          data: {
+            average_stars: +business._avg.stars.toFixed(2),
+            review_count: business._count.stars,
+          },
+        })
+      );
+    await Promise.all(update_batch);
+    console.log(
+      `Updated business # ${i} / ${business_stats.length} - ${(
+        (i / businesses.length) *
+        100
+      ).toFixed(2)}%...`
+    );
+  }
+  console.log("Business Update Complete");
 })()
   .then(async () => {
     await prisma.$disconnect();
